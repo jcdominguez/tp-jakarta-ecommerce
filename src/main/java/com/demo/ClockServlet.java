60c844759fa555f83f43bb7880e05f0fbf61dbce
jcdominguez <jcd.pro@gmail.com>
2023-10-31 11:26:42 +0100

    JSP

---
+++ src/main/java/com/demo/ClockServlet.java
@@ -0,0 +1,19 @@
+package com.demo;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@WebServlet("/clock")
+public class ClockServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+       request.getRequestDispatcher("WEB-INF/clock.jsp").forward(request, response);
+    }
+}
